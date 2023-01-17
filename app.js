d2f8cea954e5c8b3e425ca705dba65cc6ad7a5e6
RHaenszel <rhaensze@gmail.com>
2023-01-16 20:30:38 -0700

    Navbar created and added to pages with innerHTML from app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+
+
+//Update menu in index.html and copy and paste here
+document.querySelector("#navbarID").innerHTML += 
+`<div class="container">
+<a href="./index.html" class="navbar-brand active">Home</a>
+<button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navtoggle"><span class="navbar-toggler-icon"></span></button>
+<div class="collapse navbar-collapse" id="navtoggle">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a href="./container.html" class="nav-link">Containers</a>
+        </li>
+        <li class="nav-item">
+            <a href="./table.html" class="nav-link">Tables</a>
+        </li>
+        <li class="nav-item">
+            <a href="./forms.html" class="nav-link">Forms</a>
+        </li>
+        <li class="nav-item">
+            <a href="./formvalidate.html" class="nav-link">Validate</a>
+        </li>
+        <li class="nav-item">
+            <a href="./inputgroups.html" class="nav-link">InputGroups</a>
+        </li>
+        <li class="nav-item">
+            <a href="./alerts.html" class="nav-link">Alerts</a>
+        </li>
+        <li class="nav-item">
+            <a href="./buttons.html" class="nav-link">Buttons</a>
+        </li>
+        <li class="nav-item">
+            <a href="./cards.html" class="nav-link">Cards</a>
+        </li>
+        <li class="nav-item">
+            <a href="./collapse.html" class="nav-link">Collapse</a>
+        </li>
+        <li class="nav-item">
+            <a href="./modal.html" class="nav-link">Modal</a>
+        </li>
+    </ul>
+</div>
+</div>`
+
+
